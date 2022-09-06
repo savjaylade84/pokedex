ae079a46a130320c1028e71a75584a372ea460fb
savjaylade84 <savjaylade84@gmail.com>
2022-09-06 18:26:36 +0800

    added existing project

---
+++ XamarinWorkshop2/XamarinWorkshop2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinWorkshop2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private HttpClient httpClient;
+       string baseUrl = "https:/pokeapi.co/api/v2/pokemon";
+        public MainPage()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+        }
+
+        private async Task Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(pokemonEntry.Text))
+                return;
+
+            var result = await httpClient.GetStringAsync(baseUrl + pokemonEntry.Text);
+
+            if (result == null)
+            {
+
+                await DisplayAlert("pokemon not found");
+                return;
+
+            }
+
+            Pokemon pokemon = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(result);
+            pokemonImage.Source = pokemon.sprites.front_default;
+           
+        }
+    }
+}
---
+++ XamarinWorkshop2/XamarinWorkshop2/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinWorkshop2
+{
+    class Pokemon
+    {
+
+    }
+}
